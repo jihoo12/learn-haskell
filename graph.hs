0d42abcdba501c0bf7c9a89080f28c6a77f3b6cc
jihoo <jihoop0620@gmail.com>
2026-05-22 10:21:48 +0900

    update

--- graph.hs
+++ graph.hs
@@ -1,16 +1,22 @@
 type Index = Int
 type Value = String
-data Graph = 
-    Object Index Value [Index] -- Object Int(index) String(value) [list](index pointer list) 
+data Object = 
+    Object Index Value [Index] 
     deriving (Show)
-
-myobject1 :: Graph
+data Graph = 
+    Graph [Object]
+    deriving (Show)
+myobject1 :: Object
 myobject1 = Object 1 "object1" [1,4]
-myobject2 :: Graph
+myobject2 :: Object
 myobject2 = Object 2 "object2" [1,2]
-myobject3 :: Graph
+myobject3 :: Object
 myobject3 = Object 3 "object3" [1,2,3]
-myobject :: Graph
+myobject :: Object
 myobject = Object 4 "object4" [1,2,3,4]
-main = print myobject1
+mygraph :: Graph
+mygraph = Graph [myobject1,myobject2,myobject3,myobject]
+
+main :: IO ()
+main = print mygraph
 
