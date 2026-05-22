60a0fc9659bac6f13db4a49a524cbf20bcfdb664
jihoo <jihoop0620@gmail.com>
2026-05-22 11:18:44 +0900

    update

--- redblacktree.hs
+++ redblacktree.hs
@@ -3,7 +3,7 @@ type Color = Bool
 type Val = Int
 
 -- 이진 트리 구조로 수정 (왼쪽 자식, 값, 오른쪽 자식)
-data Tree = Tree Color Tree Val Tree
+data Tree = Tree Color Val Tree Tree
           | Nil
 
 -- 2번 규칙 검사: 루트는 검은색(False)이어야 한다. (빈 트리도 무조건 참)
@@ -15,9 +15,9 @@ isRootBlack (Tree color _ _ _) = color == False
 -- 즉, 현재 노드가 Red이면 두 자식은 반드시 Black(또는 Nil)이어야 함.
 noDoubleRed :: Tree -> Bool
 noDoubleRed Nil = True
-noDoubleRed (Tree True left _ right) = 
+noDoubleRed (Tree True _ left right) = 
     isBlack left && isBlack right && noDoubleRed left && noDoubleRed right
-noDoubleRed (Tree False left _ right) = 
+noDoubleRed (Tree False _ left right) = 
     noDoubleRed left && noDoubleRed right
 
 -- 보조 함수: 노드가 검은색(또는 Nil)인지 확인
